b3c1ed09dba5753e98cc3709521455241d0b7979
Kalium1993 <tassio_tln@hotmail.com>
2019-06-03 20:52:08 -0300

    Deleting unused imports

--- src/main/java/com/fundatec/AggregatorAPI/controller/AggregatorController.java
+++ src/main/java/com/fundatec/AggregatorAPI/controller/AggregatorController.java
@@ -1,19 +1,10 @@
 package com.fundatec.AggregatorAPI.controller;
 
-import java.io.IOException;
-
 import com.fundatec.AggregatorAPI.domain.*;
 import com.fundatec.AggregatorAPI.service.ClimateService;
 import com.fundatec.AggregatorAPI.service.SercretService;
 import com.fundatec.AggregatorAPI.service.ZenService;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
